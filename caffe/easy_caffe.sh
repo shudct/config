44b998d3563a49a62128a3ef3c0b3fb8ee865cf0
=Zhao.Kai <=zeakey@126.com>
2015-11-17 19:32:41 +0800

    draw_net

--- caffe/easy_caffe.sh
+++ caffe/easy_caffe.sh
@@ -3,6 +3,7 @@ sudo apt-get install git
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler 
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev 
 sudo apt-get install python-skimage python-protobuf python-numpy python-opencv python-pip
+sudo apt-get install graphviz python-pydot
 sudo pip install -r requirements.txt
 
 sudo cp -r *.conf  /etc/ld.so.conf.d/

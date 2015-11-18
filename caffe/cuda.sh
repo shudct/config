d0ee5e600ca8c4d875473dc9a75bcdb6ec632c95
Zhao.Kai <zeakey@126.com>
2015-11-18 14:54:08 +0800

    add boost

---
+++ caffe/cuda.sh
@@ -0,0 +1,3 @@
+sudo dpkg -i cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda
--- caffe/easy_caffe.sh
+++ caffe/easy_caffe.sh
@@ -1,14 +1,9 @@
 sudo apt-get update
 sudo apt-get install git
+sudo apt-get install libboost-all-dev
 sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler 
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev 
 sudo apt-get install python-skimage python-protobuf python-numpy python-opencv python-pip
 sudo apt-get install graphviz python-pydot
 sudo pip install -r requirements.txt
-
-sudo cp -r *.conf  /etc/ld.so.conf.d/
-sudo ldconfig
-
-cd /opt/ & sudo git clone https://github.com/xianyi/OpenBLAS & cd OpenBLAS & sudo make -j32 & sudo make install 
-
-cd ~/ & git clone https://github.com/BVLC/caffe & cd caffe & make -j32
+cd /opt/ & sudo git clone https://github.com/spyder-ide/spyder & sudo ln /opt/spyder/bootstrap.py /usr/local/bin/spyder

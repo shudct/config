5900eff90de299e953168173803fb66ef8e35bc4
Zhao.Kai <zeakey@126.com>
2015-11-30 14:08:10 +0800

    ok

--- caffe/easy_caffe.sh
+++ caffe/easy_caffe.sh
@@ -1,27 +1,27 @@
 OPENBLAS=1
 SPYDER=1
 NTHREAD=32
-#sudo apt-get update
-#sudo apt-get install git
-#sudo apt-get install libboost-all-dev
-#sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler 
-#sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev 
-#sudo apt-get install python-skimage python-protobuf python-numpy python-opencv python-pip
-#sudo apt-get install graphviz python-pydot
-#sudo pip install -r requirements.txt
+sudo apt-get update
+sudo apt-get install git
+sudo apt-get install libboost-all-dev
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler 
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev 
+sudo apt-get install python-skimage python-protobuf python-numpy python-opencv python-pip
+sudo apt-get install graphviz python-pydot
+sudo pip install -r requirements.txt
 
 #openblas
 if [ $OPENBLAS ]
 then
 	cd /opt/ 
-	if [ ! -d "./OpenBLAS"];then
+	if [ ! -d "./OpenBLAS" ];then
 		sudo git clone https://github.com/xianyi/OpenBLAS 
 	fi
 	cd OpenBLAS & sudo make -j$NTHREAD & sudo make install
 	sudo cp include/* /usr/local/include/
 	sudo cp lib/* /usr/local/lib/
 fi 
-if [ $SPYDER ]
+if [ $SPYDER ];then
 	cd /opt/ 
 	if [ ! -d "./spyder"];then
 		sudo git clone https://github.com/spyder-ide/spyder 
